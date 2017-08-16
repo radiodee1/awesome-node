0b9b8cb685eb58c2a571e678e55af7249f2ba6bf
David Liebman <david.c.liebman@gmail.com>
2017-08-16 09:38:56 -0400

    use pycharm

--- test_game/do_w2v_train.py
+++ test_game/do_w2v_train.py
@@ -29,23 +29,5 @@ import pandas as pd
 
 import seaborn as sns
 
-
-'''
-
-import textplayer.textPlayer as tp
-t = tp.TextPlayer('zork1.z5')
-start_info = t.run()
-print (start_info)
-
-command_in = ''
-
-while command_in != 'q':
-    command_in = raw_input("enter command :")
-    command_in = command_in.strip()
-    command_output = t.execute_command(command_in)
-    print (command_output)
-
-if t.get_score() != None:
-    score, possible_score = t.get_score()
-t.quit()
-'''
+nltk.download("punkt")
+nltk.download("stopwords")
