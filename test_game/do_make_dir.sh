39925be61743b8fce91e7a9f422b9bbdbd77ec8e
David Liebman <david.c.liebman@gmail.com>
2017-08-22 15:09:31 -0400

    starting script

--- test_game/do_make_dir.sh
+++ test_game/do_make_dir.sh
@@ -2,6 +2,9 @@ mkdir -p data trained
 echo "the data directory is for sample text"
 echo "the trained directory is for saving the trained neural net"
 echo "both are essential."
+
+git clone https://github.com/DavidGriffith/frotz.git
+git clone https://github.com/danielricks/textplayer.git
 cd textplayer
 git submodule init
 git submodule update
