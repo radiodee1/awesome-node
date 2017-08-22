e3a36a8dba9344ef0e64d98283d9ff058254588c
David Liebman <david.c.liebman@gmail.com>
2017-08-22 14:58:58 -0400

    trying to work with submodules

--- test_game/do_make_dir.sh
+++ test_game/do_make_dir.sh
@@ -2,3 +2,21 @@ mkdir -p data trained
 echo "the data directory is for sample text"
 echo "the trained directory is for saving the trained neural net"
 echo "both are essential."
+cd textplayer
+git submodule init
+git submodule update
+cd ..
+cd frotz
+git submodule init
+git submodule update
+make dumb
+cd ..
+cp -r frotz/ textplayer/frotz
+touch ./textplayer/.gitignore
+echo "frotz" >> ./textplayer/.gitignore
+echo "" >> ./textplayer/.gitignore
+echo "textplayer" >> .gitignore
+echo "" >> .gitignore
+echo "frotz" >> .gitignore
+echo "" >> .gitignore
+git rm -r --cached textplayer/
