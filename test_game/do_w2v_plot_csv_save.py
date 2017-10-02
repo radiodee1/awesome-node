3e2940ba84562f9d7b313cb2bb59123a09813034
David Liebman <david.c.liebman@gmail.com>
2017-10-02 16:51:26 -0400

    save and show csv

--- test_game/do_w2v_plot_csv_save.py
+++ test_game/do_w2v_plot_csv_save.py
@@ -22,59 +22,44 @@ print ("w2v loaded.")
 
 tsne = sklearn.manifold.TSNE(n_components=2, random_state=0)
 
-all_word_vectors_matrix = word2vec_game.wv.syn0
+all_word_vectors_matrix = []
+all_word_vectors_vocab = []
+
+if False:
+    for i in word2vec_small.wv.vocab:
+        j = word2vec_game.wv.vocab[i].index
+        all_word_vectors_matrix.append(word2vec_game.wv.syn0[j])
+        all_word_vectors_vocab.append(i)
+
+if True:
+    list_g = ['goes','gone','went','going','western','eastern','southern','northern',
+              'southerly','northerly','westerly','easterly']
+
+    list_v = ['go','west','east','north','south']
+
+    list_g.extend(list_v)
+    print (list_g)
+
+    for i in list_g:
+        j = word2vec_game.wv.vocab[i].index
+        all_word_vectors_matrix.append(word2vec_game.wv.syn0[j])
+        all_word_vectors_vocab.append(i)
+
 
 all_word_vectors_matrix_2d = tsne.fit_transform(all_word_vectors_matrix)
 
 print ("matrix 2d done.")
 
-points = pd.DataFrame(
-    [
-        (word, coords[0], coords[1])
-        for word, coords in [
-            (word, all_word_vectors_matrix_2d[word2vec_game.wv.vocab[word].index])
-            for word in word2vec_small.wv.vocab
-        ]
-    ],
-    columns=["word", "x", "y"]
-)
 
-print (points)
-#points.head[10]
 
 if True:
     f = open("trained/points.csv", "w")
     #z = 0
-    for i in word2vec_game.wv.vocab :
-        ii = word2vec_game.wv.vocab[i].index
-        print (i, all_word_vectors_matrix_2d[ii][0], all_word_vectors_matrix_2d[ii][1])
-        f.write(i +","+ str(all_word_vectors_matrix_2d[ii][0])+","+ str(all_word_vectors_matrix_2d[ii][1])+"\n")
+    for i in range(len(all_word_vectors_vocab)) :
+        ii = list(all_word_vectors_vocab)[i]
+        print (ii, all_word_vectors_matrix_2d[i][0], all_word_vectors_matrix_2d[i][1])
+        f.write(ii +","+ str(all_word_vectors_matrix_2d[i][0])+","+ str(all_word_vectors_matrix_2d[i][1])+"\n")
         #z+=1
     f.close()
 
 exit()
-
-sns.set_context("poster")
-points.plot.scatter("x", "y", s=10, figsize=(20, 12))
-
-plt.show()
-
-
-def plot_region(x_bounds, y_bounds):
-    slice = points[
-        (x_bounds[0] <= points.x) &
-        (points.x <= x_bounds[1]) &
-        (y_bounds[0] <= points.y) &
-        (points.y <= y_bounds[1])
-        ]
-    #ax = slice.plot.scatter("x", "y", s=35, figsize=(10, 8))
-
-    ax = slice.plot.scatter("x", "y", s=35, figsize=(10, 8))
-    for i, point in slice.iterrows():
-        ax.text(point.x + 0.005, point.y + 0.005, point.word, fontsize=11)
-
-plot_region(x_bounds=(-25, -20), y_bounds=(-15, -5))
-
-#plot_region(x_bounds=(-20, -12), y_bounds=(-10, -3))
-
-plt.show()
