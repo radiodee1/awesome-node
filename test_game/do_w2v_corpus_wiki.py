8c55aee1ca5430c52e8c1756a53a62859359848a
David Liebman <david.c.liebman@gmail.com>
2017-11-03 08:24:51 -0400

    try 1.4 Mil

--- test_game/do_w2v_corpus_wiki.py
+++ test_game/do_w2v_corpus_wiki.py
@@ -49,7 +49,7 @@ if __name__ == '__main__':
         if (i % 2500 == 0):
             logger.info("Saved " + str(i) + " articles")
             #index += 1
-            if index >= 20 and False: break ## 9 = 25,000 articles
+            if index >= 560 and True: break ## 560 = 1.4Mil articles
             index += 1
 
 
