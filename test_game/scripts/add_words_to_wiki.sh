4dd4e7f8350f80a007f61053a25a1568e8a76cd1
David Liebman <david.c.liebman@gmail.com>
2017-11-03 17:56:37 -0400

    edit list modifying script

--- test_game/scripts/add_words_to_wiki.sh
+++ test_game/scripts/add_words_to_wiki.sh
@@ -1,6 +1,9 @@
 echo "copy vocab list from list.txt to pre-processed wiki file."
 cat ../data/list.txt >> ../data/wiki*.txt
+echo " l u d n s w e ne sw se nw " >> ../data/wiki*.txt
 cat ../data/list.txt >> ../data/wiki*.txt
+echo " l u d n s w e ne sw se nw " >> ../data/wiki*.txt
 cat ../data/list.txt >> ../data/wiki*.txt
+echo " l u d n s w e ne sw se nw " >> ../data/wiki*.txt
 
 echo "done"
