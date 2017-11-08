5b90b229f315f215d6572d46fae10bbcc28631f4
David Liebman <david.c.liebman@gmail.com>
2017-11-08 18:06:18 -0500

    some interesting results

--- test_game/do_w2v_try_learn.py
+++ test_game/do_w2v_try_learn.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 from __future__ import absolute_import, division, print_function
+import gensim.models.word2vec as w2v
 
 import os
 import numpy as np
@@ -22,9 +23,11 @@ class LearnerModel:
         self.reg_lambda = 0.01  # regularization strength
 
         self.W1 = None
+        self.b1 = None
 
         self.start_list_len = 27
         self.epochs = 1
+        self.total_correct_old = 0
         #print (self.X)
         #print (self.y)
         self.test_zeros = False
@@ -63,20 +66,22 @@ class LearnerModel:
 
     def set_starting_list(self):
         ''' list of possible inputs to try '''
-        self.list_basic_wrong = ['goes', 'gone', 'went', 'going',
+        self.list_basic_wrong = [ #'goes', 'gone', 'went', 'going',
                   'western', 'eastern', 'southern', 'northern'
             , 'southward', 'northward', 'westward', 'eastward'
             , 'southeasterly', 'northeasterly', 'southwesterly', 'northwesterly' ## too obscure
             , 'southeastern', 'northeastern', 'southwestern', 'northwestern'
-            ,'looking','looks','looked','opens','opens','opened','opening']
+            #,'looking','looks','looked','opens','opens','opened','opening'
+        ]
 
         ''' correct outputs in order '''
-        self.list_basic_right = ['go', 'go', 'go', 'go',
+        self.list_basic_right = [ #'go', 'go', 'go', 'go',
                   'west', 'east', 'south', 'north'
             , 'south', 'north', 'west', 'east'
             , 'southeast', 'northeast', 'southwest', 'northwest' ## too obscure
             , 'southeast', 'northeast', 'southwest', 'northwest'
-            ,'look','look','look','open','open','open','open']
+            #,'look','look','look','open','open','open','open'
+        ]
 
         self.list_basic_wrong = self.list_basic_wrong[:self.start_list_len]
         self.list_basic_right = self.list_basic_right[:self.start_list_len]
@@ -87,10 +92,13 @@ class LearnerModel:
             name = "word2vec_book_vec.npy.txt"
         if os.path.isfile(os.path.join("trained", name)) and not self.test_zeros:
             odd_vec = np.loadtxt(os.path.join("trained", name))
-            #print (odd_vec)
+            odd_vec = np.array([odd_vec])
+            #print (odd_vec,"here")
+
         else:
             #odd_vec = np.zeros(300)
             odd_vec = np.random.randn(self.nn_input_dim, 300) / np.sqrt(self.nn_input_dim)
+            odd_vec.tolist()
 
         self.odd_vec = odd_vec
         return odd_vec
@@ -103,8 +111,9 @@ class LearnerModel:
             np.savetxt(os.path.join('trained',name), odd_vec)
 
     # Helper function to evaluate the total loss on the dataset
-    def calculate_loss(self, model):
+    def calculate_loss(self, model, sample=None):
         W1, b1 = model['W1'], model['b1']
+        #W1 = W1 + sample
         # Forward propagation to calculate our predictions
         z1 = self.X.dot(W1) + b1
 
@@ -132,71 +141,112 @@ class LearnerModel:
 
 
 
-    def build_model(self, num_features=300, num_passes=1, print_loss=False):
+    def build_model(self, num_features=300, num_passes=1, print_loss=False, game_ref=None):
         # Initialize the parameters to random values. We need to learn these.
         np.random.seed(0)
         W1 = np.random.randn(self.nn_input_dim, num_features) / np.sqrt(self.nn_input_dim)
         b1 = np.zeros((1, num_features))
 
-        print (W1)
-        print (self.W1)
+        total_correct = 0
+
+        #print (W1)
+        #print (self.W1)
+
         if not self.W1 is None:
-            W1 = list(self.W1)
+            W1 =  np.array(self.W1)
+
 
         # This is what we return at the end
         model = {}
 
         # Gradient descent. For each batch...
-        for i in range(0, num_passes):
+        for i in range(0, self.start_list_len):
+
+            try:
+                sample = game_ref.word2vec_book.wv[self.list_basic_wrong[i]]
+                #print (self.list_basic_right[i], self.list_basic_wrong[i])
+                pass
+            except: # NameError:
+                continue
 
             # Forward propagation
-            z1 = self.X.dot(W1) # + b1
-            z1 = z1 + b1
+            #W1 = W1 + sample
+            z1 = self.X.dot(W1) + b1
+
             exp_scores = np.exp(z1)
             probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
 
             # Backpropagation
             delta = probs
-            delta[range(self.num_examples), self.y] -= 1
+            delta[range(self.start_list_len), self.y] -= 1
 
             dW1 = np.dot(self.X.T, delta)
             db1 = np.sum(delta, axis=0)
 
             # Add regularization terms (b1 and b2 don't have regularization terms)
-            dW1 += self.reg_lambda * W1
+            dW1 += self.reg_lambda * W1 # np.array(W1)
 
             # Gradient descent parameter update
             W1 += -self.epsilon * dW1
             b1 += -self.epsilon * db1
 
+            #W1 = dW1
+
             # Assign new parameters to the model
             model = {'W1': W1, 'b1': b1}
             self.W1 = W1
+            #self.b1 = b1
 
             # Optionally print the loss.
             # This is expensive because it uses the whole dataset, so we don't want to do it too often.
             if print_loss and i % 1000 == 0:
-                print("Loss after iteration %i: %f" % (i, self.calculate_loss(model)))
+                print("Loss after iteration %i: %f" % (i, self.calculate_loss(model,sample=sample)))
+
+            score = self.check_odd_vector(game_ref, odd_vec=self.W1 - sample
+                                          , debug_msg=False
+                                          , list_try=self.list_basic_wrong
+                                          , list_correct=self.list_basic_right)
+
+            #if score == 1.0: total_correct += 1
+            total_correct = score * self.start_list_len
+
+            if total_correct > self.total_correct_old:
+                #total_correct = total_correct + score #* self.start_list_len
+                self.total_correct_old = total_correct
+                self.save_vec(odd_vec=self.W1 - sample)
+                print("--->", end="")
 
         return model
 
-    def generate_perfect_vector(self, game):
+    def generate_perfect_vector(self, game, debug_msg=False):
         g = game
-        for i in range(self.epochs):
-            for j in range(self.start_list_len):
-                score = self.check_odd_vector(g, odd_vec=self.W1,debug_msg=True
-                                             , list_try=[self.list_basic_wrong[j]]
-                                             , list_correct=[self.list_basic_right[j]])
-                if score == 1.0:
-                    print (score)
-                    self.y = np.array([int(score)])
-                else:
-                    self.y = np.array([0])
+        total_tested = self.start_list_len
+        #total_correct = 0
+        self.total_correct_old = 0
 
-                self.X = np.array([[1]])
-                self.num_examples = 1
-                model = l.build_model(print_loss=True,num_passes=1)
+        X = []
+        y = []
+        for x in range(len(self.list_basic_right)):
+            if not self.list_basic_right[x] == self.list_basic_wrong[x]:
+                y.append([1])
+            else:
+                y.append([0])
+            X.append([1])
             pass
+        self.X = np.array(X)
+        self.y = np.array([y])
+
+        for i in range(self.epochs):
+            total_correct = 0
+            #for j in range(1):
+
+            model = l.build_model(print_loss=True,num_passes=self.start_list_len,game_ref=g)
+
+
+            print ("totals",self.total_correct_old / total_tested, self.total_correct_old, total_tested)
+            total_correct = 0
+            pass
+
 
         pass
 
@@ -207,15 +257,34 @@ if __name__ == "__main__":
 
     l = LearnerModel()
 
-    l.W1 = l.load_vec()
+    l.W1 = l.load_vec().tolist()
     l.set_starting_list()
-    '''
-    l.X = np.array([[1]])
-    l.y = np.array([1])
-    l.num_examples = len(list(l.X))  # training set size
-    '''
-    #model = l.build_model(print_loss=True)
+    l.start_list_len = len(l.list_basic_right)
 
+    l.epochs = 200
     l.generate_perfect_vector(game)
-    # Build a model
-    #model = build_model(print_loss=True)
+
+    print ("----------------")
+    if True:
+
+        l.W1 = l.load_vec().tolist()
+        score = l.check_odd_vector(game, odd_vec=l.W1
+                                  , debug_msg=True
+                                  , list_try=l.list_basic_wrong
+                                  , list_correct=l.list_basic_right)
+        print ("----------------")
+        for i in range(l.start_list_len):
+            try:
+                sample = game.word2vec_book.wv[l.list_basic_wrong[i]]
+                #print (self.list_basic_right[i], self.list_basic_wrong[i])
+                pass
+            except: # NameError:
+                continue
+
+            score = l.check_odd_vector(game, odd_vec=l.W1 - sample
+                                       , debug_msg=False
+                                       , list_try=[l.list_basic_wrong[i]]
+                                       , list_correct=[l.list_basic_right[i]])
+
+    if False:
+        print (l.W1)
