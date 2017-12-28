a3bf57311155cf3b4fa7be5c08bdd7c255e91dc4
David Liebman <david.c.liebman@gmail.com>
2017-12-28 10:24:00 -0500

    separate out speech-out class -- try google sr with game

--- test_game/game_sr.py
+++ test_game/game_sr.py
@@ -3,35 +3,6 @@
 from __future__ import division, print_function
 
 
-#!/usr/bin/env python
-
-# Copyright 2017 Google Inc. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-"""Google Cloud Speech API sample application using the streaming API.
-
-NOTE: This module requires the additional dependency `pyaudio`. To install
-using pip:
-
-    pip install pyaudio
-
-Example usage:
-    python transcribe_streaming_mic.py
-"""
-
-# [START import_libraries]
-
 import re
 import sys
 
@@ -241,6 +212,6 @@ class VoiceGoogleSR:
 
 if __name__ == '__main__':
     v = VoiceGoogleSR()
-    for i in range(3):
+    for i in range(3):  # test three iterations
         words = v.voice_detection()
         print(words)
