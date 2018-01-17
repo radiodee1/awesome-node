ba9fff886ccb4280cc581e13e75f76905f1defa1
David Liebman <david.c.liebman@gmail.com>
2018-01-17 12:09:56 -0500

    readme

--- test_mnist_nmt/do_make_dir.sh
+++ test_mnist_nmt/do_make_dir.sh
@@ -2,3 +2,13 @@ git clone https://github.com/tensorflow/nmt/
 
 mkdir data/ model/ raw/
 touch nmt/__init__.py
+
+if [ -d raw/sequences  ] ; then
+    echo "sequences file exists"
+else
+
+    wget https://github.com/edwin-de-jong/mnist-digits-stroke-sequence-data/raw/master/sequences.tar.gz
+    tar vxzr sequences.tar.gz
+    echo "sequences file created"
+
+fi
---
+++ test_mnist_nmt/readme.md
@@ -0,0 +1,7 @@
+# Notes:
+
+see this link and the following download:
+
+https://github.com/edwin-de-jong/mnist-digits-stroke-sequence-data/wiki/MNIST-digits-stroke-sequence-data
+
+wget https://github.com/edwin-de-jong/mnist-digits-stroke-sequence-data/raw/master/sequences.tar.gz
