e188d76a85040468a017c67d49f61ca9e62635e8
David Liebman <david.c.liebman@gmail.com>
2018-01-17 12:48:47 -0500

    make test and train

--- test_mnist_nmt/do_make_dir.sh
+++ test_mnist_nmt/do_make_dir.sh
@@ -6,9 +6,10 @@ touch nmt/__init__.py
 if [ -d raw/sequences  ] ; then
     echo "sequences file exists"
 else
-
+    cd raw/
     wget https://github.com/edwin-de-jong/mnist-digits-stroke-sequence-data/raw/master/sequences.tar.gz
-    tar vxzr sequences.tar.gz
+    
+    tar vxzf sequences.tar.gz
     echo "sequences file created"
-
+    cd ..
 fi
--- test_mnist_nmt/do_make_train_test.py
+++ test_mnist_nmt/do_make_train_test.py
@@ -2,6 +2,9 @@
 
 import os
 from tensorflow.examples.tutorials.mnist import input_data
+import csv
+
+
 mnist = input_data.read_data_sets("raw", one_hot=False)
 
 d = 0.5
@@ -22,35 +25,45 @@ def add_to_file(in_line, out_line, name):
         f.write(out_line+'\n')
     pass
 
-def look_at_image(mnist_part, name='', short=False, batch=1000, subbatch=10):
+def look_at_image(mnist_part, name='', short=False, batch=1000, subbatch=10, sequence=False):
     for ii in range(batch):
         batch_xs, batch_ys = mnist_part.next_batch(subbatch)
         print(ii, 'out of', batch , name)
         for i in range(len(batch_xs)):
             s = ''
-            for j in range(w):
-                for k in range(w):
-                    z = 0
-                    if batch_xs[i][j * w + k] > d:
-                        z = 1
-                    if  short: print(z, end='')
-                    if z == 1: 
-                        if both_coords:
-                            # two coords
-                            s += ' ' + '/' + str(k) + '/' + str(j) + '/'
-                        else:
-                            # one coord
-                            s += ' ' + '/' + str(k) + '/'
-                if  short: print('|')
+            if not sequence:
+                for j in range(w):
+                    for k in range(w):
+                        z = 0
+                        if batch_xs[i][j * w + k] > d:
+                            z = 1
+                        if  short: print(z, )
+                        if z == 1:
+                            if both_coords:
+                                # two coords
+                                s += ' ' + '/' + str(k) + '/' + str(j) + '/'
+                            else:
+                                # one coord
+                                s += ' ' + '/' + str(k) + '/'
+                    if  short: print('|')
+            else:
+                with open('raw/sequences/'+ name +'img-'+ str(i) + '-points.txt', 'r') as f:
+                    ff = csv.reader(f)
+                    for row in ff:
+                        if row[1] != 'row' and row[0] != 'col' and int(row[0]) >= 0 and int(row[1]) >= 0:
+                            s += ' ' + '/' + str(row[1]) + '/' + str(row[0]) + '/'
+                    #print(s)
+                    #print(ff[1])
+                    pass
             if  short: print(name + ' val=', batch_ys[i])
             if not short: add_to_file(s, str(batch_ys[i]), name)
             if i > 3 and short: break
         if short: break
     
-look_at_image(mnist.train, name='train',short=False, batch=5500)
+look_at_image(mnist.train, name='train',short=False, batch=5500, sequence=True)
 
-look_at_image(mnist.test, name='test', short=False, batch=1000)
+look_at_image(mnist.test, name='test', short=False, batch=1000, sequence=True)
 
-look_at_image(mnist.validation, name='validation', short=False, batch=500)
+#look_at_image(mnist.validation, name='validation', short=False, batch=500)
 
 os.system('mv train.from train.to test.from test.to validation.from validation.to data/.')
