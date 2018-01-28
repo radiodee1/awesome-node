0eb8cecc79fa4d250bba964fcf7d9b49580d5302
David Liebman <david.c.liebman@gmail.com>
2018-01-28 10:42:01 -0500

    try to save as txt

---
+++ test_game/do_w2v_save_as_text.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+from __future__ import absolute_import, division, print_function
+import codecs
+
+import glob
+
+import logging
+
+import multiprocessing
+
+import os
+
+import pprint
+import io
+import re
+
+import nltk
+from nltk.tokenize import TweetTokenizer, sent_tokenize, PunktSentenceTokenizer
+from nltk.stem import *
+import gensim.models.word2vec as w2v
+
+if True:
+    print("stage: load model")
+    word2vec_book = w2v.Word2Vec.load(os.path.join("trained", "word2vec_book.w2v"))
+    word2vec_book.save(os.path.join("trained", "word2vec_book.w2v.txt"))
